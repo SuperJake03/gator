00c0e638c9290d43fad7a23a728b26c7b1f798c1
Superjake03 <jacoblopez0308@gmail.com>
2026-07-20 17:32:01 -0500

    reset command

--- handler_user.go
+++ handler_user.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
+	"log"
+	"time"
+
+	"github.com/SuperJake03/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -15,8 +19,7 @@ func handlerLogin(s *state, cmd command) error {
 
 	_, err := s.db.GetUser(context.Background(), userName)
 	if err != nil {
-		fmt.Println("couldn't find user")
-		os.Exit(1)
+		return fmt.Errorf("couldn't find user: %w", err)
 	}
 
 	if err := s.cfg.SetUser(userName); err != nil {
@@ -24,5 +27,40 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Println("User switched successfully!")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	userName := cmd.Args[0]
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      userName,
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("couldn't create user: %w", err)
+	}
+
+	if err := s.cfg.SetUser(userName); err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User was created!")
+	log.Printf("User created: %v+", user)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if err := s.db.DeleteUsers(context.Background()); err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+
+	fmt.Println("Database reset successfully!")
 	return nil
 }
